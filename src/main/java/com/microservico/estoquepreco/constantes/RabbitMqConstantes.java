b4000efa83ec49129d7082629b8fbda60897431d
Vinicius.Leite <viniciusthiago2@hotmail.com>
2022-10-26 15:08:02 -0300

    Remocao de classe de constantes.

--- src/main/java/com/microservico/estoquepreco/constantes/RabbitMqConstantes.java
+++
@@ -1,9 +0,0 @@
-package com.microservico.estoquepreco.constantes;
-
-public class RabbitMqConstantes {
-
-    public static final String FILA_ESTOQUE = "ESTOQUE";
-
-    public static final String FILA_PRECO = "PRECO";
-
-}
